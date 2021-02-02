fe9b4fe992573597bb1296652addb09bdca9f04c
user <christiansanders1337@gmail.com>
2021-02-02 01:46:48 -0500

    Car tab is done

--- CarXLoot/usrMods.cs
+++ CarXLoot/usrMods.cs
@@ -17,7 +17,7 @@ namespace usrMods
         public static bool colorTrans = false;
         public static bool lerpColorWindow1 = false;
         public static bool lerpColorWindow2 = false;
-        public static bool carClicked = false;
+        public static bool carClicked = true;
         public static bool gameClicked = false;
         public static bool settingsClicked = false;
         public static float rbSpeed = 1.0F;
@@ -39,6 +39,7 @@ namespace usrMods
         private Font consolas;
         public GUISkin TextBottomSkin;
         public GUISkin TextSkin;
+        public GUISkin TextCenterSkin;
         public GUISkin WindowSkin;
         public GUISkin CheckBoxCheckSkin;
         public GUISkin CheckBoxUncheckSkin;
@@ -58,10 +59,17 @@ namespace usrMods
         private Texture2D CAR_BUTTON_OFF = LoadTexture("carbuttonoff.png");
         private Texture2D SETTINGS_BUTTON_ON = LoadTexture("settingsbuttonon.png");
         private Texture2D SETTINGS_BUTTON_OFF = LoadTexture("settingsbuttonoff.png");
+        private Texture2D GAME_BUTTON_ON = LoadTexture("gamebuttonon.png");
+        private Texture2D GAME_BUTTON_OFF = LoadTexture("gamebuttonoff.png");
         private Texture2D BUTTON = LoadTexture("button.png");
         private Texture2D BUTTON_PRESSED = LoadTexture("buttonclicked.png");
         private Texture2D SLIDER = LoadTexture("slider.png");
         private Texture2D SLIDER_THUMB = LoadTexture("sliderthing.png");
+        public Rect menu_area = new Rect(50, 20, 756, 420);
+        private const int ELEMENT_SIZE = 40;
+        private const int SPACING = 15;
+        private const int GAP = 10;
+        private int element_y;
 
         //funcs
         public static Texture2D MakeTex(int width, int height, Color col)
@@ -88,18 +96,79 @@ namespace usrMods
             return texture2D;
         }
 
+        public bool AddButton()
+        {
+            Rect r = new Rect(menu_area.xMin + GAP, element_y, menu_area.xMax - GAP * 2, ELEMENT_SIZE);
+            element_y = (int)r.yMax + SPACING;
+            return GUI.Button(r, "");
+        }
+
 
         public void Start()
         {
-            //usrObject = new UnityEngine.GameObject();
-            //usrObject.AddComponent<GUIS>();
-            //UnityEngine.Object.DontDestroyOnLoad(usrObject);
-            //GameObject guisObject = GameObject.Find("usrObject");
-            //GUIS guiSkins = guisObject.GetComponent<GUIS>();
+            this.consolas = Font.CreateDynamicFontFromOSFont("Consolas", 18);
+            TextBottomSkin = ScriptableObject.CreateInstance<GUISkin>();
+            TextSkin = ScriptableObject.CreateInstance<GUISkin>();
+            TextCenterSkin = ScriptableObject.CreateInstance<GUISkin>();
+            WindowSkin = ScriptableObject.CreateInstance<GUISkin>();
+            CheckBoxCheckSkin = ScriptableObject.CreateInstance<GUISkin>();
+            CheckBoxUncheckSkin = ScriptableObject.CreateInstance<GUISkin>();
+            CarOnSkin = ScriptableObject.CreateInstance<GUISkin>();
+            CarOffSkin = ScriptableObject.CreateInstance<GUISkin>();
+            SettingsOnSkin = ScriptableObject.CreateInstance<GUISkin>();
+            SettingsOffSkin = ScriptableObject.CreateInstance<GUISkin>();
+            GameOnSkin = ScriptableObject.CreateInstance<GUISkin>();
+            GameOffSkin = ScriptableObject.CreateInstance<GUISkin>();
+            ButtonSkin = ScriptableObject.CreateInstance<GUISkin>();
+            SliderSkin = ScriptableObject.CreateInstance<GUISkin>();
+            SliderThumbSkin = ScriptableObject.CreateInstance<GUISkin>();
+            this.TextSkin.label.font = this.consolas;
+            this.TextSkin.label.fontSize = 13;
+            this.TextSkin.label.normal.textColor = Color.white;
+            this.TextCenterSkin.label.font = this.consolas;
+            this.TextCenterSkin.label.fontSize = 13;
+            this.TextCenterSkin.label.normal.textColor = Color.white;
+            this.TextCenterSkin.label.alignment = TextAnchor.MiddleCenter;
+            this.TextBottomSkin.label.font = this.consolas;
+            this.TextBottomSkin.label.fontSize = 10;
+            this.WindowSkin.box.normal.background = this.MENU_BG;
+            this.WindowSkin.box.font = this.consolas;
+            this.WindowSkin.box.normal.textColor = Color.white;
+            this.WindowSkin.box.fontSize = 15;
+            this.WindowSkin.box.alignment = TextAnchor.UpperCenter;
+            this.CheckBoxCheckSkin.button.normal.background = this.CHECK;
+            this.CheckBoxCheckSkin.button.stretchHeight = false;
+            this.CheckBoxCheckSkin.button.stretchWidth = false;
+            this.CheckBoxCheckSkin.button.fixedHeight = 20f;
+            this.CheckBoxCheckSkin.button.fixedWidth = 20f;
+            this.CheckBoxUncheckSkin.button.normal.background = this.UNCHECK;
+            this.CheckBoxUncheckSkin.button.stretchHeight = false;
+            this.CheckBoxUncheckSkin.button.stretchWidth = false;
+            this.CheckBoxUncheckSkin.button.fixedHeight = 20f;
+            this.CheckBoxUncheckSkin.button.fixedWidth = 20f;
+            this.CarOnSkin.button.normal.background = this.CAR_BUTTON_ON;
+            this.CarOffSkin.button.normal.background = this.CAR_BUTTON_OFF;
+            this.SettingsOnSkin.button.normal.background = this.SETTINGS_BUTTON_ON;
+            this.SettingsOffSkin.button.normal.background = this.SETTINGS_BUTTON_OFF;
+            this.GameOnSkin.button.normal.background = this.GAME_BUTTON_ON;
+            this.GameOffSkin.button.normal.background = this.GAME_BUTTON_OFF;
+            this.ButtonSkin.button.normal.background = this.BUTTON;
+            this.ButtonSkin.button.active.background = this.BUTTON_PRESSED;
+            this.ButtonSkin.button.font = this.consolas;
+            this.ButtonSkin.button.normal.textColor = Color.white;
+            this.ButtonSkin.button.alignment = TextAnchor.MiddleCenter;
+            this.SliderSkin.horizontalSlider.normal.background = this.SLIDER;
+            this.SliderSkin.horizontalSlider.stretchHeight = false;
+            this.SliderSkin.horizontalSlider.stretchWidth = false;
+            this.SliderSkin.horizontalSlider.fixedHeight = 20f;
+            this.SliderSkin.horizontalSliderThumb.normal.background = this.SLIDER_THUMB;
+            this.SliderSkin.horizontalSliderThumb.stretchHeight = false;
+            this.SliderSkin.horizontalSliderThumb.stretchWidth = false;
+            this.SliderSkin.horizontalSliderThumb.fixedHeight = 20f;
+            this.SliderSkin.horizontalSliderThumb.fixedWidth = 6f;
         }
         public void Update()
         {
-            // GameConsole.Print(skin.ToString());
             bool keyDown2 = Input.GetKeyDown(KeyCode.Delete);
             if (keyDown2)
                 uiToggle.showUi = !uiToggle.showUi;
@@ -117,99 +186,129 @@ namespace usrMods
 
         public void OnGUI()
         {
-            this.consolas = Font.CreateDynamicFontFromOSFont("Consolas", 12);
-            TextBottomSkin = ScriptableObject.CreateInstance<GUISkin>();
-            TextSkin = ScriptableObject.CreateInstance<GUISkin>();
-            WindowSkin = ScriptableObject.CreateInstance<GUISkin>();
-            CheckBoxCheckSkin = ScriptableObject.CreateInstance<GUISkin>();
-            CheckBoxUncheckSkin = ScriptableObject.CreateInstance<GUISkin>();
-            CarOnSkin = ScriptableObject.CreateInstance<GUISkin>();
-            CarOffSkin = ScriptableObject.CreateInstance<GUISkin>();
-            SettingsOnSkin = ScriptableObject.CreateInstance<GUISkin>();
-            SettingsOffSkin = ScriptableObject.CreateInstance<GUISkin>();
-            GameOnSkin = ScriptableObject.CreateInstance<GUISkin>();
-            GameOffSkin = ScriptableObject.CreateInstance<GUISkin>();
-            ButtonSkin = ScriptableObject.CreateInstance<GUISkin>();
-            SliderSkin = ScriptableObject.CreateInstance<GUISkin>();
-            SliderThumbSkin = ScriptableObject.CreateInstance<GUISkin>();
-            this.TextSkin.label.font = this.consolas;
-            this.TextSkin.label.fontSize = 13;
-            this.TextBottomSkin.label.font = this.consolas;
-            this.TextBottomSkin.label.fontSize = 10;
-            this.WindowSkin.box.normal.background = this.MENU_BG;
-            this.CheckBoxCheckSkin.button.normal.background = this.CHECK;
-            this.CheckBoxCheckSkin.button.stretchHeight = false;
-            this.CheckBoxCheckSkin.button.stretchWidth = false;
-            this.CheckBoxCheckSkin.button.fixedHeight = 20f;
-            this.CheckBoxCheckSkin.button.fixedWidth = 20f;
-            this.CheckBoxUncheckSkin.button.normal.background = this.UNCHECK;
-            this.CheckBoxUncheckSkin.button.stretchHeight = false;
-            this.CheckBoxUncheckSkin.button.stretchWidth = false;
-            this.CheckBoxUncheckSkin.button.fixedHeight = 20f;
-            this.CheckBoxUncheckSkin.button.fixedWidth = 20f;
-            this.CarOnSkin.button.normal.background = this.CAR_BUTTON_ON;
-            this.CarOffSkin.button.normal.background = this.CAR_BUTTON_OFF;
-            this.SettingsOnSkin.button.normal.background = this.SETTINGS_BUTTON_ON;
-            this.SettingsOffSkin.button.normal.background = this.SETTINGS_BUTTON_OFF;
-            this.GameOnSkin.button.normal.background = this.SETTINGS_BUTTON_ON;
-            this.GameOffSkin.button.normal.background = this.SETTINGS_BUTTON_OFF;
-            this.ButtonSkin.button.normal.background = this.BUTTON;
-            this.ButtonSkin.button.active.background = this.BUTTON_PRESSED;
-            this.ButtonSkin.button.font = this.consolas;
-            this.ButtonSkin.button.normal.textColor = Color.white;
-            this.SliderSkin.horizontalSlider.normal.background = this.SLIDER;
-            this.SliderSkin.horizontalSlider.stretchHeight = false;
-            this.SliderSkin.horizontalSlider.stretchWidth = false;
-            this.SliderSkin.horizontalSlider.fixedHeight = 20f;
-            this.SliderSkin.horizontalSliderThumb.normal.background = this.SLIDER_THUMB;
-            this.SliderSkin.horizontalSliderThumb.stretchHeight = false;
-            this.SliderSkin.horizontalSliderThumb.stretchWidth = false;
-            this.SliderSkin.horizontalSliderThumb.fixedHeight = 20f;
-            this.SliderSkin.horizontalSliderThumb.fixedWidth = 6f;
             if (uiToggle.showUi)
             {
-                // ---CAR TAB---
+                element_y = (int)menu_area.yMin + GAP;
+                GameInput.InputManager.LockInput(this);
                 GUI.skin = WindowSkin;
-                GUI.Box(new Rect(50, 20, 756, 420), ""); // rect is x, y, w, h
+                GUI.Box(menu_area, "");
 
-                if (!carClicked)
+                if (gameClicked == false && settingsClicked == false)
                 {
-                    GUI.skin = CarOffSkin;
-                    bool cbclicked = GUI.Button(new Rect(66, 25, 247, 41), "");
-                    if (cbclicked)
-                    {
-                        carClicked = !carClicked;
-                    }
+                    carClicked = true;
                 }
-                
-                if (carClicked)
+
+                // ---CAR TAB---
+                GUI.skin = carClicked ? CarOnSkin : CarOffSkin;
+                if (GUI.Button(new Rect(56, 25, 247, 41), ""))
                 {
-                    GUI.skin = CarOnSkin;
-                    bool cbclicked = GUI.Button(new Rect(66, 25, 247, 41), "");
-                    if (cbclicked)
-                    {
-                        carClicked = !carClicked;
-                    }
+                    carClicked = !carClicked;
+                    gameClicked = false;
+                    settingsClicked = false;
                 }
 
                 // ---GAME TAB---
-                //GUI.skin = this.skin.GameOffSkin;
-                //GUI.Button(new Rect(315, 25, 247, 41), "");
-                //GUI.skin = this.skin.GameOnSkin;
-                //GUI.Button(new Rect(315, 25, 247, 41), "");
+                GUI.skin = gameClicked ? GameOnSkin : GameOffSkin;
+                if (GUI.Button(new Rect(305, 25, 247, 41), ""))
+                {
+                    gameClicked = !gameClicked;
+                    carClicked = false;
+                    settingsClicked = false;
+                }
 
                 // ---SETTINGS TAB---
-                //GUI.skin = this.skin.SettingsOffSkin;
-                //GUI.Button(new Rect(564, 25, 247, 41), "");
-                //GUI.skin = this.skin.SettingsOnSkin;
-                //GUI.Button(new Rect(564, 25, 247, 41), "");
+                GUI.skin = settingsClicked ? SettingsOnSkin : SettingsOffSkin;
+                if(GUI.Button(new Rect(554, 25, 248, 41), ""))
+                {
+                    settingsClicked = !settingsClicked;
+                    carClicked = false;
+                    gameClicked = false;
+                }
 
-                //lerpButt1.normal.background = MakeTex(30, 30, smokeLerp1);
-                //lerpButt2.normal.background = MakeTex(30, 30, smokeLerp2);
+                lerpButt1.normal.background = MakeTex(30, 30, smokeLerp1);
+                lerpButt2.normal.background = MakeTex(30, 30, smokeLerp2);
+
+                if (carClicked)
+                {
+                    //RAINBOW
+                    GUIStyle localStyle = new GUIStyle(GUI.skin.label);
+                    localStyle.normal.textColor = Color.white;
+                    localStyle.fontSize = 12;
+                    localStyle.font = consolas;
+                    GUI.skin = rbSmokeBool ? CheckBoxCheckSkin : CheckBoxUncheckSkin;
+                    if(GUI.Button(new Rect(76, 85, 20, 20), ""))
+                        {
+                            rbSmokeBool = !rbSmokeBool;
+                            colorTrans = false;
+                        }
+                    GUI.skin = SliderSkin;
+                    GUI.Label(new Rect(101, 88, 20, 100), "Rainbow Tire Smoke", localStyle);
+                    rbSpeed = GUI.HorizontalSlider(new Rect(76, 110, 150, 15), rbSpeed, 0.5f, 10f);
+                    GUI.Label(new Rect(230, 113, 20, 100), "Rainbow Cycle Speed + [" + rbSpeed.ToString("0.0") + "]", localStyle);
+                    //LERP
+                    GUI.skin = colorTrans ? CheckBoxCheckSkin : CheckBoxUncheckSkin;
+                    if (GUI.Button(new Rect(76, 140, 20, 20), ""))
+                    {
+                        colorTrans = !colorTrans;
+                        rbSmokeBool = false;
+                    }
+                    GUI.skin = SliderSkin;
+                    GUI.Label(new Rect(101, 143, 20, 100), "Gradient Smoke", localStyle);
+                    lerpSpeed = GUI.HorizontalSlider(new Rect(76, 165, 150, 15), lerpSpeed, 0.5f, 10f);
+                    GUI.Label(new Rect(230, 168, 20, 100), "Gradient Speed [" + lerpSpeed.ToString("0.0") + "]", localStyle);
+                    if(GUI.Button(new Rect(226, 140, 20, 20), "", lerpButt1))
+                    {
+                        lerpColorWindow1 = !lerpColorWindow1;
+                        lerpColorWindow2 = false;
+                    }
+                    if(lerpColorWindow1)
+                    {
+                        GUI.skin = WindowSkin;
+                        Rect lerpWindow1 = new Rect(274, 140, 250, 250);
+                        GUI.Box(lerpWindow1, "Gradient Color 1");
+                        GUI.skin = SliderSkin;
+                        lRed1 = GUI.HorizontalSlider(new Rect(334, 190, 120, 20), lRed1, 0f, 1f);
+                        lGreen1 = GUI.HorizontalSlider(new Rect(334, 256, 120, 20), lGreen1, 0f, 1f);
+                        lBlue1 = GUI.HorizontalSlider(new Rect(334, 322, 120, 20), lBlue1, 0f, 1f);
+                        GUI.skin = TextCenterSkin;
+                        GUI.Label(new Rect(355, 214, 80, 12), "Red");
+                        GUI.Label(new Rect(355, 280, 80, 12), "Green");
+                        GUI.Label(new Rect(355, 346, 80, 12), "Blue");
+                        smokeLerp1 = new Color(lRed1, lGreen1, lBlue1);
+                        GUI.skin = ButtonSkin;
+                        if (GUI.Button(new Rect(504, 140, 20, 20), "x"))
+                            lerpColorWindow1 = false;
+                    }
+                    if(GUI.Button(new Rect(249, 140, 20, 20), "", lerpButt2))
+                    {
+                        lerpColorWindow2 = !lerpColorWindow2;
+                        lerpColorWindow1 = false;
+                    }
+                    if (lerpColorWindow2)
+                    {
+                        GUI.skin = WindowSkin;
+                        Rect lerpWindow2 = new Rect(274, 140, 250, 250);
+                        GUI.Box(lerpWindow2, "Gradient Color 2");
+                        GUI.skin = SliderSkin;
+                        lRed2 = GUI.HorizontalSlider(new Rect(334, 190, 120, 20), lRed2, 0f, 1f);
+                        lGreen2 = GUI.HorizontalSlider(new Rect(334, 256, 120, 20), lGreen2, 0f, 1f);
+                        lBlue2 = GUI.HorizontalSlider(new Rect(334, 322, 120, 20), lBlue2, 0f, 1f);
+                        GUI.skin = TextCenterSkin;
+                        GUI.Label(new Rect(355, 214, 80, 12), "Red");
+                        GUI.Label(new Rect(355, 280, 80, 12), "Green");
+                        GUI.Label(new Rect(355, 346, 80, 12), "Blue");
+                        smokeLerp2 = new Color(lRed2, lGreen2, lBlue2);
+                        GUI.skin = ButtonSkin;
+                        if (GUI.Button(new Rect(504, 140, 20, 20), "x"))
+                            lerpColorWindow2 = false;
+                    }
+                    
+                }
+
+
                 //UIHelper.Begin("/usr/'s CarX Stuff", 5 + xOffset, 5 + yOffset, 300, 600, 15, 20, 2/*, GUIStyle.none*/);
                 //UIHelper.Label("");
-
-                //if (UIHelper.Button("RB Smoke ", rbSmokeBool/*, GUIStyle.none*/))
+                //if (UIHelper.Button("RB Smoke ", rbSmokeBool))
                 //{
                 //    if (!colorTrans)
                 //    {
@@ -299,7 +398,7 @@ namespace usrMods
                 //UIHelper.Label("Y Offset");
                 //yOffset = UIHelper.Slider(yOffset, 0, 475);
             }
-
+            else {GameInput.InputManager.ResetLockInput(this);}
         }
         public class uiToggle
         {
